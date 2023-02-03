a206685555edb7699918f40f6234109da59c24cd
Zerebel <86872654+Zerebel@users.noreply.github.com>
2023-02-03 07:31:09 +0000

    modules installed and configured

---
+++ todo/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="w-screen h-screen bg-green-400"></div>;
+}
+
+export default App;
---
+++ todo/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
